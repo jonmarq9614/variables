465d752f895592135edf71fa70745cce3fe4d861
Jonathan Marquez <jmarqu12@icloud.com>
2021-04-08 08:36:31 -0600

    module 5

--- variables/wwwroot/js/varScript.js
+++ variables/wwwroot/js/varScript.js
@@ -1,6 +1,23 @@
-﻿var myInt = 1;
-var myString = "javascript";
-var myString2 = 'JavaScript';
-var myBool = true;
+﻿var myDate = new Date(1996, 6, 11);
+var myDate2 = convertToStringFormat(myDate);
 
-console.log(myInt, myString, myString2, myBool);
+var newDate = new Date(1996, 6, 11);
+document.write(newDate.toLocaleString("fr-FR"));
+
+document.write(myDate2);
+
+function convertToStringFormat(myDate) {
+
+    var dateString =
+        ((myDate.getMonth() + 1).toString()
+            .padStart(2, "0")
+            +
+            "/" +
+            myDate.getDate().toString().padStart(2, "0") +
+            "/" +
+        myDate.getFullYear());
+
+
+    return dateString;
+
+}
